5003cd8411e4792d63951eaab2c3f84fa47926b7
YannGabriel <yanngabrieldev@gmail.com>
2025-09-22 12:24:36 -0300

    Update: Create new tests and update database connection

--- Makefile
+++ Makefile
@@ -1,17 +1,17 @@
 postgres:
-	docker run --name flint-container -p 5432:5432 -e POSTGRES_USER=flint-admin -e POSTGRES_PASSWORD=root -d postgres:latest
+	docker run --name fint-container -p 5432:5432 -e POSTGRES_USER=fint-admin -e POSTGRES_PASSWORD=root -d postgres:latest
 
 createdb:
-	docker exec -it flint-container createdb --username=flint-admin --owner=flint-admin fint-database
+	docker exec -it fint-container createdb --username=fint-admin --owner=fint-admin fint-db
 
 dropdb:
-	docker exec -it flint-container dropdb --username=flint-admin fint-database
+	docker exec -it fint-container dropdb --username=fint-admin fint-db
 
 migrateup:
-	migrate -path db/migration -database 'postgres://flint-admin:root@localhost:5432/fint-database?sslmode=disable' -verbose up
+	migrate -path db/migration -database 'postgres://fint-admin:root@localhost:5432/fint-db?sslmode=disable' -verbose up
 
 migratedown:
-	migrate -path db/migration -database 'postgres://flint-admin:root@localhost:5432/fint-database?sslmode=disable' -verbose down
+	migrate -path db/migration -database 'postgres://fint-admin:root@localhost:5432/fint-db?sslmode=disable' -verbose down
 
 sqlc:
 	sqlc generate
--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -3,13 +3,15 @@ package database
 import (
 	"testing"
 	"context"
-	"github.com/YannGabriel/fint-api/utils"
+	"fmt"
+
 	"github.com/stretchr/testify/require"
+
+	"github.com/YannGabriel/fint-api/utils"
 )
 
-func TestCreateUser (t *testing.T){
+func CreateRandomUser(t *testing.T) User{
 	Username := utils.RandomUsername();
-
 
 	user, err := testQueries.CreateUser(context.Background(), Username);
 
@@ -19,4 +21,58 @@ func TestCreateUser (t *testing.T){
 
 	require.NotZero(t, user.ID)
 	require.NotZero(t, user.CreatedAt)
+	fmt.Println("Usuário criado:", user)
+
+	return user
 }
+
+
+func TestCreateUser (t *testing.T){
+	CreateRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	user1 := CreateRandomUser(t)
+	user2, err := testQueries.GetUserByID(context.Background(), user1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.Username, user2.Username)
+}
+
+func TestListUsers (t *testing.T){
+	_ = CreateRandomUser(t)
+
+	users, err := testQueries.ListUsers(context.Background())
+	require.NoError(t, err)
+	require.NotEmpty(t, users)
+}
+
+func TestDeleteUser (t *testing.T){
+	user1 := CreateRandomUser(t)
+	err := testQueries.DeleteUser(context.Background(), user1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user1)
+
+}
+
+func TestUpdateUser (t *testing.T){
+	user1 := CreateRandomUser(t)
+
+	arg := UpdateUserParams{
+		ID: user1.ID,
+		Username: "TESTE DE UPDATE",
+	}
+
+	user2, err := testQueries.UpdateUser(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, arg.Username, user2.Username)
+	require.Equal(t, user1.ID, user2.ID)
+}
+
